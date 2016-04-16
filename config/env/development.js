b6e3270236ee14529db3b1f0bd25295a91ebe135
Nicholas Crumrine <uncletammy@yahoo.com>
2016-04-16 03:58:39 -0500

    increased orm timeout

--- config/env/development.js
+++ config/env/development.js
@@ -16,7 +16,9 @@ module.exports = {
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-
+   orm: {
+   	timeout: 40000
+   }
   // models: {
   //   connection: 'someMongodbServer'
   // }
