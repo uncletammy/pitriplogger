7e4ac992fe39640918a8c0c07e697d71d72c4b4a
Nicholas Crumrine <uncletammy@yahoo.com>
2016-04-16 00:48:53 -0500

    added cam and gps hook

---
+++ api/hooks/cam.js
@@ -0,0 +1,15 @@
+
+module.exports = function cam(sails) {
+  return {
+
+    initialize: function (done) {
+      sails.after('hook:orm:loaded', function() {
+        // setupServer(function(){
+          sails.log('Camera ready!');
+        // });
+      });
+      return done();
+    },
+    camera: {}
+  };
+};
---
+++ api/hooks/gps.js
@@ -0,0 +1,41 @@
+var Bancroft = require('bancroft');
+var gps;
+
+
+var savePoint = function(point){
+  var coordinates = point&&point.geometries&&point.geometries.coordinates;
+  sails.log('saving point:',coordinates);
+  return;
+};
+
+var setupGps = function(callback){
+  gps = new Bancroft();
+
+  gps.on('satellite', function (satellite) {
+    sails.log('New Satellite:',satellite);
+  });
+
+  gps.on('disconnect', function (err) {
+    sails.log('Disconnected:',arguments);
+  });
+
+  gps.on('location',savePoint);
+  gps.on('connect', callback);
+
+};
+
+
+module.exports = function gps(sails) {
+  return {
+    initialize: function (done) {
+      sails.after('hook:orm:loaded', function() {
+        setupGps(function(){
+          sails.log('GPS ready!');
+          sails.log(arguments);
+        });
+      });
+      return done();
+    },
+    gpsObject: gps
+  };
+};
